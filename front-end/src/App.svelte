470809c060f64e72fc414ddaf80f53d2a6d49f69
ElMoustacho <pikachu2154@gmail.com>
2024-09-26 20:13:58 +0200

    Appli Svelte de base

---
+++ front-end/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import logoBDS from "/logo-bds.png";
+</script>
+
+<main>
+	<div>
+		<img src={logoBDS} alt="Logo BDS" draggable="false" />
+	</div>
+
+	<h1>Bientôt, le site du BDS ici 🗿</h1>
+</main>
+
+<style>
+	:global(*) {
+		user-select: none;
+	}
+
+	img {
+		width: 20rem;
+	}
+</style>

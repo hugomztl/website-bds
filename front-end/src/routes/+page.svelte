1dd47520fa5bdd7a659b853a160ef6b688bb5bfd
ElMoustacho <pikachu2154@gmail.com>
2024-09-30 12:21:56 +0200

    Suppression lien invalide

--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -3,9 +3,7 @@
 		<img src="logo-bds.png" alt="Logo BDS" draggable="false" />
 	</div>
 
-	<a href="/test3">
-		<h1>Bientôt, le site du BDS ici 🗿</h1>
-	</a>
+	<h1>Bientôt, le site du BDS ici 🗿</h1>
 </main>
 
 <style>

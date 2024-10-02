842f7d63ca7878f1cae65864ea39a19829d282f1
ElMoustacho <pikachu2154@gmail.com>
2024-10-02 23:14:17 +0200

    Ajout d'une page d'erreur custom

---
+++ svelte-kit/src/routes/+error.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<main>
+	<h1>Erreur <span class="error-code">{$page.status}</span></h1>
+	<p>Une erreur s'est produite : <b>{$page.error?.message}</b></p>
+	<a class="home" href="/">Retour à l'accueil</a>
+</main>
+
+<style>
+	main {
+		margin: auto;
+	}
+
+	h1,
+	p {
+		font-weight: 400;
+	}
+
+	.error-code {
+		color: coral;
+		font-weight: bold;
+	}
+
+	.home {
+		color: blueviolet;
+		font-weight: bold;
+		text-decoration: underline;
+	}
+</style>

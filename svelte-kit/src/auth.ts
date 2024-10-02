efdf6fe8e8833392011a4d67aa666f0951b88dd8
ElMoustacho <pikachu2154@gmail.com>
2024-10-02 19:40:03 +0200

    Ajout de la propriété "license" pour les users

--- svelte-kit/src/auth.ts
+++ svelte-kit/src/auth.ts
@@ -3,7 +3,32 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/sveltekit/providers/github';
 import { client } from './lib/database';
 
+declare module '@auth/sveltekit' {
+	interface User {
+		license: boolean;
+	}
+}
+
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	providers: [GitHub],
-	adapter: MongoDBAdapter(client)
+	adapter: MongoDBAdapter(client),
+	callbacks: {
+		async signIn({ user }) {
+			if (!('emailVerified' in user)) {
+				console.error('User is not AdapterUser.');
+				return false;
+			}
+
+			// On ajoute les propriétés supplémentaires si elles n'existent pas
+			const existingUser = await client.db().collection('users').findOne({ email: user.email });
+			user.license = existingUser?.license ?? false;
+
+			// On pourra vérifier si le mail de l'utilisateur n'est pas (via)cesi, et refuser la connexion
+			return true;
+		},
+		// Ce callback permet d'assigner correctement le nouveau modèle
+		session({ session }) {
+			return session;
+		}
+	}
 });

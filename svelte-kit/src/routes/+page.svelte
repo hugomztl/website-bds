6442369fa050dc2f928e1ff6a709535100748d91
ElMoustacho <pikachu2154@gmail.com>
2024-10-02 10:48:13 +0200

    🗿

---
+++ svelte-kit/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<main id="app">
+	<div>
+		<img src="logo-bds.png" alt="Logo BDS" draggable="false" />
+	</div>
+
+	<h1>Bientôt, le site du BDS ici 🗿</h1>
+</main>
+
+<style>
+	:global(*) {
+		user-select: none;
+	}
+
+	img {
+		width: 20rem;
+	}
+</style>

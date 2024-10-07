e22e81554931a7a562a958765921fce8e37091ce
ElMoustacho <pikachu2154@gmail.com>
2024-10-07 16:57:18 +0200

    Désactive le bouton pour joindre si l'utilisateur n'est pas login

--- svelte-kit/src/routes/clubs/[id=mongoId]/+page.svelte
+++ svelte-kit/src/routes/clubs/[id=mongoId]/+page.svelte
@@ -13,7 +13,9 @@
 	{:else if data.member?.pending === true}
 		<p>Tu es en attente de validation.</p>
 	{:else}
-		<form method="POST" action="?/join" use:enhance><button>Rejoindre le club</button></form>
+		<form method="POST" action="?/join" use:enhance>
+			<button disabled={!data.member}>Rejoindre le club</button>
+		</form>
 	{/if}
 
 	<h2>Derniers posts du club</h2>

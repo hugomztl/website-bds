85811775be7bb3eea54cd4a08e28671d3eb880fa
ElMoustacho <pikachu2154@gmail.com>
2024-10-07 11:46:57 +0200

    Clubs cliquables

--- svelte-kit/src/routes/clubs/+page.svelte
+++ svelte-kit/src/routes/clubs/+page.svelte
@@ -14,34 +14,35 @@
 	<h1>Clubs:</h1>
 
 	{#each data.clubs as club}
-		<div class="club">
-			{#if canManageClub(club)}
-				<div class="club-header">
-					<a href={`/clubs/manage/${club._id}`}>
-						<svg
-							class="pen-icon"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							width="24"
-							height="24"
-						>
-							<path fill="none" d="M0 0h24v24H0z" />
-							<path
-								d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"
-							/>
-						</svg>
-					</a>
-				</div>
-			{/if}
-			<h2>{club.name}</h2>
-
-			<p>{club.description}</p>
-			{#if club.owner}
-				<p>Président: {club.owner.fullName}</p>
-			{:else}
-				<p>Actuellement sans président</p>
-			{/if}
-		</div>
+		<a href={`/clubs/${club._id}`}>
+			<div class="club">
+				{#if canManageClub(club)}
+					<div class="club-header">
+						<a href={`/clubs/manage/${club._id}`}>
+							<svg
+								class="pen-icon"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								width="24"
+								height="24"
+							>
+								<path fill="none" d="M0 0h24v24H0z" />
+								<path
+									d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"
+								/>
+							</svg>
+						</a>
+					</div>
+				{/if}
+				<h2>{club.name}</h2>
+				<p>{club.description}</p>
+				{#if club.owner}
+					<p>Président: {club.owner.fullName}</p>
+				{:else}
+					<p>Actuellement sans président</p>
+				{/if}
+			</div>
+		</a>
 	{:else}
 		<h2>Aucun club pour le moment</h2>
 	{/each}
@@ -62,5 +63,19 @@
 		display: flex;
 		justify-content: end;
 		align-items: center;
+	}
+
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
+
+	a > div {
+		transition: 0.3s ease-in-out;
+	}
+
+	a:hover > div {
+		transform: scale(1.05);
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 	}
 </style>

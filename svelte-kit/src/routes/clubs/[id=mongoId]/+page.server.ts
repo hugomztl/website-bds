499838c6b48a6f878849752a222626bfd5e9880e
ElMoustacho <pikachu2154@gmail.com>
2024-10-07 15:33:52 +0200

    Possibilité de se mettre en attente pour rejoindre un club

--- svelte-kit/src/routes/clubs/[id=mongoId]/+page.server.ts
+++ svelte-kit/src/routes/clubs/[id=mongoId]/+page.server.ts
@@ -1,15 +1,40 @@
 import { recursiveStringifyId } from '$lib/database.js';
 import Club from '$lib/models/Club';
-import { error } from '@sveltejs/kit';
+import { error, fail } from '@sveltejs/kit';
+import { addUserToClub } from './util';
+import mongoose from 'mongoose';
 
-export const load = async ({ params }) => {
-	const club = await Club.findById(params.id).lean().exec();
+export const prerender = false;
+
+export const actions = {
+	join: async ({ locals, params }) => {
+		const session = await locals.auth();
+		if (!session) {
+			return fail(403, { message: 'Vous devez être connecté pour rejoindre un club.' });
+		}
+
+		const club = await Club.findById(params.id).exec();
+		if (!club) {
+			throw error(404, 'Club introuvable.');
+		}
+
+		addUserToClub(mongoose.Types.ObjectId.createFromHexString(session.user?.id!), club);
+	}
+};
+
+export const load = async ({ params, locals }) => {
+	const session = await locals.auth();
+
+	const club = await Club.findById(params.id).exec();
 
 	if (!club) {
 		throw error(404, 'Club introuvable.');
 	}
 
 	return {
-		club: recursiveStringifyId(club)
+		club: recursiveStringifyId(club.toObject()),
+		member: club.members
+			.find((member) => (!session ? session : member.user?.equals(session.user?.id!)))
+			?.toObject({ flattenObjectIds: true })
 	};
 };
--- svelte-kit/src/routes/clubs/[id=mongoId]/+page.svelte
+++ svelte-kit/src/routes/clubs/[id=mongoId]/+page.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+
 	export let data;
 </script>
 
 <main id="app">
 	<h1>{data.club.name}</h1>
 	<p>{data.club.description}</p>
+
+	{#if data.member?.pending === false}
+		<p>Tu es dans ce club!</p>
+	{:else if data.member?.pending === true}
+		<p>Tu es en attente de validation.</p>
+	{:else}
+		<form method="POST" action="?/join" use:enhance><button>Rejoindre le club</button></form>
+	{/if}
 
 	<h2>Derniers posts du club</h2>
 	{#each data.club.posts as post}

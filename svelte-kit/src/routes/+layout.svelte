26d1b4e5c84d75bf2292a8b4e01f1f3c9fa4253a
ElMoustacho <pikachu2154@gmail.com>
2024-10-07 19:58:58 +0200

    Ajout de la création de posts de blog

--- svelte-kit/src/routes/+layout.svelte
+++ svelte-kit/src/routes/+layout.svelte
@@ -1,8 +1,18 @@
+<script>
+	import { isAdmin } from '$lib/authutil';
+
+	export let data;
+</script>
+
 <nav>
 	<a href="/">Accueil</a>
 	<a href="/clubs">Clubs</a>
 	<a href="/signin">Connexion</a>
 	<a href="/register">Inscription</a>
+
+	{#if isAdmin(data.session)}
+		<a class="adminLink" href="/blog">Gestion du blog</a>
+	{/if}
 </nav>
 
 <slot />
@@ -16,5 +26,10 @@
 		background-color: #f0f0f0;
 		width: 100%;
 		gap: 1rem;
+	}
+
+	.adminLink {
+		margin-left: auto;
+		color: chocolate;
 	}
 </style>
---
+++ svelte-kit/src/routes/blog/+page.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+
+	export let data;
+	$: posts = data.posts;
+</script>
+
+<h1>Gestion du blog</h1>
+
+<form method="POST" action="?/createPost" use:enhance>
+	<h2>Créer un nouveau post</h2>
+	<div>
+		<label for="title">Titre:</label>
+		<input type="text" id="title" name="title" required />
+	</div>
+	<div>
+		<label for="content">Contenu:</label>
+		<textarea id="content" name="content" required></textarea>
+	</div>
+	<button type="submit">Créer le post</button>
+</form>
+
+{#each posts as post}
+	<div>
+		<h2>{post.title}</h2>
+		<p>{post.content}</p>
+	</div>
+{:else}
+	<h2>Le BDS n'a encore rien posté 😵</h2>
+{/each}
+
+<style>
+	form {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	form div {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+	}
+
+	form button {
+		align-self: flex-start;
+	}
+
+	div {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	textarea {
+		resize: vertical;
+		min-height: 100px;
+		min-width: 100%;
+		padding: 0.5rem;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		font-family: inherit;
+		font-size: 1rem;
+	}
+</style>

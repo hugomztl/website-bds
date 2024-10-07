c6d5a29ba48f294bd1b35893f39651383713b232
ElMoustacho <pikachu2154@gmail.com>
2024-10-07 11:24:43 +0200

    Ajout d'une barre de nav pour le dev

---
+++ svelte-kit/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<nav>
+	<a href="/">Accueil</a>
+	<a href="/clubs">Clubs</a>
+	<a href="/signin">Connexion</a>
+	<a href="/register">Inscription</a>
+</nav>
+
+<slot />
+
+<style>
+	nav {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		padding: 1rem;
+		background-color: #f0f0f0;
+		width: 100%;
+		gap: 1rem;
+	}
+</style>

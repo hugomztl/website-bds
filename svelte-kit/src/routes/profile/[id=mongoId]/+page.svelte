4af415130c83b1235f15baf093f14d3faf72e9ce
ElMoustacho <pikachu2154@gmail.com>
2024-10-11 00:48:14 +0200

    Ajout d'un bouton d'achat de la FFSU

--- svelte-kit/src/routes/profile/[id=mongoId]/+page.svelte
+++ svelte-kit/src/routes/profile/[id=mongoId]/+page.svelte
@@ -43,6 +43,31 @@
 	{/if}
 
 	<p><b>Licencié:</b> {user.license ? 'Oui ✔' : 'Non ❌'}</p>
+	{#if !user.license && data.session?.user?.id === user._id.toString()}
+		<a href="/ffsu">
+			<button
+				class="btn flex transform items-center gap-2 rounded-md bg-blue-500 px-4 py-2 font-semibold text-white shadow-md transition duration-300 ease-in-out hover:bg-blue-600"
+			>
+				Acheter ma licence FFSU
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="inline-block"
+				>
+					<circle cx="9" cy="21" r="1"></circle>
+					<circle cx="20" cy="21" r="1"></circle>
+					<path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+				</svg>
+			</button>
+		</a>
+	{/if}
 
 	<p><b>Email:</b> {user.email}</p>
 

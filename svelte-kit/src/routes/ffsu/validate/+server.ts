7eea75c4aa872ca7c86dbba5e32cd0bf5e82d847
ElMoustacho <pikachu2154@gmail.com>
2024-10-11 00:31:50 +0200

    Supprime le paymentIntent s'il y a eu une erreur

--- svelte-kit/src/routes/ffsu/validate/+server.ts
+++ svelte-kit/src/routes/ffsu/validate/+server.ts
@@ -4,20 +4,19 @@ import User from '$lib/models/User';
 import { json, redirect } from '@sveltejs/kit';
 
 export const GET = async ({ url }) => {
-	if (url.searchParams.get('code') !== 'succeeded') {
-		return json({ error: 'Payment failed' }, { status: 400 });
-	}
-
 	const intentId = url.searchParams.get('checkoutIntentId');
 	if (!intentId) {
 		return json({ error: 'Intent ID is required' }, { status: 400 });
 	}
 
-	console.info('Trying to validate payment with intent id: ', intentId);
-
 	const paymentIntent = await PaymentIntent.findOne({ intentId });
 	if (!paymentIntent) {
 		return json({ error: 'Payment intent not found' }, { status: 404 });
+	}
+
+	if (url.searchParams.get('code') !== 'succeeded') {
+		await paymentIntent.deleteOne();
+		return json({ error: 'Payment failed' }, { status: 400 });
 	}
 
 	const user = await User.findById(paymentIntent.user);
@@ -31,8 +30,6 @@ export const GET = async ({ url }) => {
 	await user.save();
 
 	await paymentIntent.deleteOne();
-
-	console.info('Validated payment for user: ', user.id);
 
 	return redirect(303, '/ffsu');
 };

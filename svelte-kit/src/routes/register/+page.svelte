1252047ee9b66e00512eb2ded24ceb85ff169bba
ElMoustacho <pikachu2154@gmail.com>
2024-10-13 20:55:23 +0200

    Connecte l'utilisateur après avoir créé son compte

--- svelte-kit/src/routes/register/+page.svelte
+++ svelte-kit/src/routes/register/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import { signIn } from '@auth/sveltekit/client';
 
 	let email = $page.url.searchParams.get('email') || '';
 </script>
@@ -12,7 +14,20 @@
 		class="mt-6 flex w-[25%] flex-col items-center"
 		action="/register"
 		method="POST"
-		use:enhance
+		use:enhance={() => {
+			return async ({ update, formData }) => {
+				const signInResult = await signIn('credentials', {
+					email: formData.get('email'),
+					password: formData.get('password')
+				});
+
+				if (signInResult?.ok) {
+					goto('/');
+				} else {
+					console.error("Une erreur s'est produite lors de la connexion.");
+				}
+			};
+		}}
 	>
 		{#if $page.form?.taken}
 			<p class="alert variant-filled-error mb-6 p-4 px-6">

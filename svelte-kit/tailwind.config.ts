1bd69364cd9413171c605b2bfe81c115a9cf3e4f
ElMoustacho <pikachu2154@gmail.com>
2024-10-14 11:07:13 +0200

    Suppression animations tailwind

--- svelte-kit/tailwind.config.ts
+++ svelte-kit/tailwind.config.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
-import { bdsTheme } from './bdstheme'
+import { bdsTheme } from './bdstheme';
 
 // 1. Import the Skeleton plugin
 import { skeleton } from '@skeletonlabs/tw-plugin';
@@ -13,44 +13,6 @@ const config = {
 		// 3. Append the path to the Skeleton package
 		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
-	theme: {
-		extend: {
-			keyframes: {
-				glitch: {
-					'0%, 100%': { transform: 'translate(0)' },
-					'20%': { transform: 'translate(-2px, -2px)' },
-					'40%': { transform: 'translate(2px, 2px)' },
-					'60%': { transform: 'translate(-2px, 2px)' },
-					'80%': { transform: 'translate(2px, -2px)' },
-				  },
-				  glitchTop: {
-					'0%, 100%': { transform: 'translate(0)' },
-					'20%': { transform: 'translate(2px, -2px)' },
-					'40%': { transform: 'translate(-2px, 2px)' },
-					'60%': { transform: 'translate(2px, 2px)' },
-					'80%': { transform: 'translate(-2px, -2px)' },
-				  },
-				  glitchBottom: {
-					'0%, 100%': { transform: 'translate(0)' },
-					'20%': { transform: 'translate(-2px, 2px)' },
-					'40%': { transform: 'translate(2px, -2px)' },
-					'60%': { transform: 'translate(-2px, -2px)' },
-					'80%': { transform: 'translate(2px, 2px)' },
-				  },
-				  shake: {
-					'0%, 100%': { transform: 'translate(0, 0)' },
-					'10%, 30%, 50%, 70%, 90%': { transform: 'translate(-10px, -10px)' },
-					'20%, 40%, 60%, 80%': { transform: 'translate(10px, 10px)' },
-				  }
-				},
-				animation: {
-				  glitch: 'glitch 1s infinite',
-				  glitchTop: 'glitchTop 1s infinite',
-				  glitchBottom: 'glitchBottom 1s infinite',
-				  shake: 'shake 1s infinite',
-				}
-		    }
-	},
 	plugins: [
 		forms,
 		// 4. Append the Skeleton plugin (after other plugins)
@@ -59,13 +21,10 @@ const config = {
 				preset: [
 					{
 						name: 'skeleton',
-						enhancements: true,
+						enhancements: true
 					}
 				],
-				custom: [
-					bdsTheme
-					
-				]
+				custom: [bdsTheme]
 			}
 		})
 	]

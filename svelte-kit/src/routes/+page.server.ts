6fa7fbdf8b15b74fd29887d66130c5eb0d59b3b1
ElMoustacho <pikachu2154@gmail.com>
2024-10-24 16:15:13 +0200

    Chargement des évènements sur la page d'accueil

--- svelte-kit/src/routes/+page.server.ts
+++ svelte-kit/src/routes/+page.server.ts
@@ -1,15 +1,27 @@
 import BlogPost from '$lib/models/BlogPost';
+import type { EventType } from '$lib/models/Event';
 
-export async function load() {
+export async function load({ fetch }) {
 	try {
 		const posts = await BlogPost.find().sort({ createdAt: -1 });
+		const events: EventType[] = await (
+			await fetch(
+				'/api/event?' +
+					new URLSearchParams({
+						date: new Date().toISOString(),
+						range: 10 + ''
+					})
+			)
+		).json();
 		return {
-			posts: posts.map((post) => post.toObject({ flattenObjectIds: true }))
+			posts: posts.map((post) => post.toObject({ flattenObjectIds: true })),
+			events
 		};
 	} catch (error) {
-		console.error('Error fetching blog posts:', error);
+		console.error('Error fetching blog posts or events:', error);
 		return {
-			posts: []
+			posts: [],
+			events: []
 		};
 	}
 }

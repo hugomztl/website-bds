2589f8741588238b096facbed666c42170473028
ElMoustacho <pikachu2154@gmail.com>
2024-11-01 15:03:43 +0100

    Ajout boutons de suppression

--- svelte-kit/src/routes/dashboard/events/[id=mongoId]/+page.svelte
+++ svelte-kit/src/routes/dashboard/events/[id=mongoId]/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+	import { page } from '$app/stores';
 	import zEvent from '$lib/models/schemas/zEvent.js';
+	import { Trash } from 'lucide-svelte';
 	import { superForm } from 'sveltekit-superforms';
 	import { zod } from 'sveltekit-superforms/adapters';
 	import EventForm from '../EventForm.svelte';
@@ -20,8 +23,23 @@
 
 <div class="container mx-auto">
 	<EventForm superForm={_superForm} title="Modifier un évènement" id={data.event?._id}>
-		<button slot="button" type="submit" class="btn variant-filled-success variant-outline-primary">
-			Modifier l'évènement
-		</button>
+		<div class="btn-group" slot="button">
+			<button type="submit" class="btn variant-filled-success variant-outline-primary">
+				Modifier l'évènement
+			</button>
+			<form
+				action="/dashboard/events/?/deleteEvent"
+				method="POST"
+				use:enhance={({ cancel }) => {
+					if (!confirm('Voulez-vous vraiment supprimer cet évènement ?')) cancel();
+				}}
+			>
+				<input type="hidden" name="id" value={$page.params.id} />
+				<button class="btn variant-filled-error variant-outline-primary">
+					<Trash />
+					<span>Supprimer l'évènement</span>
+				</button>
+			</form>
+		</div>
 	</EventForm>
 </div>

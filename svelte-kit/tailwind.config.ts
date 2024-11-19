4c00b423b2640e1f3575aa1a636c7fcc16dc2d09
ElMoustacho <pikachu2154@gmail.com>
2024-11-19 10:19:34 +0100

    Ajout plugin tailwind typography

--- svelte-kit/tailwind.config.ts
+++ svelte-kit/tailwind.config.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
 import { bdsTheme } from './bdstheme';
 
 // 1. Import the Skeleton plugin
@@ -15,6 +16,7 @@ const config = {
 	],
 	plugins: [
 		forms,
+		typography,
 		// 4. Append the Skeleton plugin (after other plugins)
 		skeleton({
 			themes: {

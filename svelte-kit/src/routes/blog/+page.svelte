04e1cf1a18a65814c58b616ccb38c8570601d89b
hugomztl <hugo.mazzitelli@viacesi.fr>
2025-01-17 19:40:52 +0100

    Enhance blog post pages with improved layout, author details, and navigation buttons

--- svelte-kit/src/routes/blog/+page.svelte
+++ svelte-kit/src/routes/blog/+page.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Card from "$lib/components/ui/card";
 	import * as Table from "$lib/components/ui/table";
+	import { ChevronRight } from 'lucide-svelte';
 	export let data;
 	$: posts = data.posts;
 </script>
 
-<h1 class="h1 my-8 text-center">Blog du BDS</h1>
-
-<div class="container mx-auto mt-10 flex flex-col gap-8">
+<div class="container mx-auto mt-[5%] flex flex-col gap-8">
 	{#each posts as post}
 	<Card.Root>
 		<Card.Header>
 		  <Card.Title>{post.title}</Card.Title>
-		  <Card.Description>Posté le {post.createdAt.toLocaleDateString()}</Card.Description>
+		  <Card.Description>Posté le {post.createdAt.toLocaleDateString()} par {post.createdBy}</Card.Description>
 		</Card.Header>
 		<Card.Content>
-		  <p>{post.content}</p>
+		  <p>{@html post.content}</p>
 		</Card.Content>
-		<Card.Footer>
-		  <p>Card Footer</p>
+		<Card.Footer class="flex justify-end">
+		  <Button href={`/blog/${post._id}`} variant="secondary">Voir plus<ChevronRight class="ml-1"/></Button>
 		</Card.Footer>
 	</Card.Root>
 	  
--- svelte-kit/src/routes/blog/[id=mongoId]/+page.svelte
+++ svelte-kit/src/routes/blog/[id=mongoId]/+page.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
+    
+    import Button from "$lib/components/ui/button/button.svelte";
+import * as Card from "$lib/components/ui/card/index.js";
+	import Separator from "$lib/components/ui/separator/separator.svelte";
+	import { ChevronLeft } from "lucide-svelte";
     export let data;
 
-    let post = data.post;
+    $: post = data.post;
+
 </script>
 
-<article>
-    <h1>{post.title}</h1>
-    <p><em>{new Date(post.postedAt).toLocaleDateString()}</em></p>
-    <div>{@html post.content}</div>
+<section class="container mt-[5%] grid grid-cols-5 grid-row-1 gap-4">
+    <Button href="/blog" variant="secondary" class="h-full"><ChevronLeft class="mr-1"/>Retour</Button>
+    <div class="col-span-4">
+        <Card.Title>{post.title}</Card.Title>
+        <Separator class="my-4"/>
+        <Card.Description>Posté le {new Date(post.createdAt).toLocaleDateString()} par {post.createdBy}</Card.Description>
+    </div>
+</section>
+
+<article class="container mt-4">
+   
+    <Card.Root class="mt-4">
+        <Card.Content>
+            
+                <div>{@html post.content}</div>
+
+        </Card.Content>
+        <Card.Footer>
+
+        </Card.Footer>
+            
+    </Card.Root>
 </article>

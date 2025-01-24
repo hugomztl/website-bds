52f461e78750adae257e1289d7f5cd3d85a422f1
hugomztl <hugo.mazzitelli@viacesi.fr>
2025-01-24 11:15:34 +0100

    Refactor dashboard layout and enhance event display with card components

--- svelte-kit/src/routes/dashboard/+page.svelte
+++ svelte-kit/src/routes/dashboard/+page.svelte
@@ -33,89 +33,82 @@
 	}
 </script>
 
-<Tabs.Root value="dashboard" class="w-[400px] mt-[5%]">
-	<Tabs.List>
-	  <Tabs.Trigger value="dashboard">Apperçu</Tabs.Trigger>
-	  <Tabs.Trigger value="eventManage">Évènements</Tabs.Trigger>
-	  <Tabs.Trigger value="userManage">Utilisateurs</Tabs.Trigger>
-	  <Tabs.Trigger value="clubManage">Clubs</Tabs.Trigger>
-	  <Tabs.Trigger value="blogManage">Blogs</Tabs.Trigger>
-	</Tabs.List>
-	<Tabs.Content value="dashboard">
-	  Statistiques ici
-	</Tabs.Content>
-	<Tabs.Content value="eventManage">Gestion des events ici</Tabs.Content>
-	<Tabs.Content value="userManage">Gestion des utilisateurs ici</Tabs.Content>
-	<Tabs.Content value="clubManage">Gestion des clubs ici</Tabs.Content>
-	<Tabs.Content value="blogManage">Gestion des blogs ici</Tabs.Content>
-</Tabs.Root>
-
-<h1 class="h1">Tableau de bord</h1>
-
-<div class="mgap-4 grid grid-cols-1 grid-rows-1 md:grid-cols-2">
-	<div
-		class="rounded-container-token bg-surface-400-500-token text-on-surface-token relative col-span-1 row-span-1 m-4 p-4 shadow-md"
-	>
-		<h2 class="h2">Évènements</h2>
-
-		<Button href="/dashboard/events/create" variant="ghost">
-			<Plus class="mr-1" />Nouvel évènement
-		</Button>
-
-		<ul class="list">
-			{#each events as event}
-				<Event {event}/>
-			{:else}
-				Aucun évènement!{/each}
-		</ul>
-
-		<h2 class="h2">Évènements passés</h2>
-
-		<ul class="list">
-			{#each pastEvents as event}
-				<Event {event}/>
-			{:else}
-				Aucun évènement passé!{/each}
-		</ul>
-	</div>
-
-	<div
-		class="rounded-container-token bg-surface-400-500-token text-on-surface-token relative col-span-1 row-span-1 m-4 p-4 shadow-md"
-	>
-		<h2 class="h2 mb-4">Blog</h2>
-
-		<a href="/dashboard/blog/create" class="btn variant-filled-primary">Rédiger un nouveau post</a>
-
-		<div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
-			{#each posts as blogPost}
-				<div>
-					<div
-						class="rounded-container-token bg-surface-600-300-token flex max-h-48 flex-col overflow-clip text-ellipsis !rounded-b-none p-2 shadow-sm"
-					>
-						<h3>{blogPost.title}</h3>
-						<p>Publié le {formatDate(blogPost.createdAt)}</p>
-						<p>
-							Par <a
-								class="anchor text-primary-400-500-token"
-								href="/profile/{blogPost.createdBy._id}">{blogPost.createdBy.fullName}</a
-							>
-						</p>
-						<p class="blog-content overflow-y-auto overflow-x-hidden pr-4 text-justify">
-							{blogPost.content}
-						</p>
-					</div>
-					<div class="rounded-container-token btn-group variant-filled flex w-full !rounded-t-none">
-						<a href="dashboard/blog/{blogPost._id}" class="btn flex-grow">Modifier</a>
-						<button class="btn flex-grow" on:click={() => deletePost(blogPost)}>Supprimer</button>
-					</div>
+<div class="container mt-[5%]">
+	<Tabs.Root value="dashboard">
+		<Tabs.List>
+		  <Tabs.Trigger value="dashboard">Apperçu</Tabs.Trigger>
+		  <Tabs.Trigger value="eventManage">Évènements</Tabs.Trigger>
+		  <Tabs.Trigger value="userManage">Utilisateurs</Tabs.Trigger>
+		  <Tabs.Trigger value="clubManage">Clubs</Tabs.Trigger>
+		  <Tabs.Trigger value="blogManage">Blogs</Tabs.Trigger>
+		</Tabs.List>
+		<Tabs.Content value="dashboard">
+		  Statistiques ici
+		</Tabs.Content>
+		<Tabs.Content value="eventManage">
+				<div
+					class="rounded-container-token bg-surface-400-500-token text-on-surface-token relative col-span-1 row-span-1 m-4 p-4 shadow-md"
+				>
+					<h2 class="h2">Évènements</h2>
+			
+					<Button href="/dashboard/events/create" variant="ghost">
+						<Plus class="mr-1" />Nouvel évènement
+					</Button>
+			
+					<ul class="list">
+						{#each events as event}
+							<Event {event}/>
+						{:else}
+							Aucun évènement!{/each}
+					</ul>
+			
+					<h2 class="h2">Évènements passés</h2>
+			
+					<ul class="list">
+						{#each pastEvents as event}
+							<Event {event}/>
+						{:else}
+							Aucun évènement passé!{/each}
+					</ul>
 				</div>
-			{/each}
+		</Tabs.Content>
+		<Tabs.Content value="userManage">Gestion des utilisateurs ici</Tabs.Content>
+		<Tabs.Content value="clubManage">Gestion des clubs ici</Tabs.Content>
+		<Tabs.Content value="blogManage">
+			<div
+			class="rounded-container-token bg-surface-400-500-token text-on-surface-token relative col-span-1 row-span-1 m-4 p-4 shadow-md"
+		>
+			<h2 class="h2 mb-4">Blog</h2>
+	
+			<a href="/dashboard/blog/create" class="btn variant-filled-primary">Rédiger un nouveau post</a>
+	
+			<div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+				{#each posts as blogPost}
+					<div>
+						<div
+							class="rounded-container-token bg-surface-600-300-token flex max-h-48 flex-col overflow-clip text-ellipsis !rounded-b-none p-2 shadow-sm"
+						>
+							<h3>{blogPost.title}</h3>
+							<p>Publié le {formatDate(blogPost.createdAt)}</p>
+							<p>
+								Par <a
+									class="anchor text-primary-400-500-token"
+									href="/profile/{blogPost.createdBy._id}">{blogPost.createdBy.fullName}</a
+								>
+							</p>
+							<p class="blog-content overflow-y-auto overflow-x-hidden pr-4 text-justify">
+								{blogPost.content}
+							</p>
+						</div>
+						<div class="rounded-container-token btn-group variant-filled flex w-full !rounded-t-none">
+							<a href="dashboard/blog/{blogPost._id}" class="btn flex-grow">Modifier</a>
+							<button class="btn flex-grow" on:click={() => deletePost(blogPost)}>Supprimer</button>
+						</div>
+					</div>
+				{/each}
+			</div>
 		</div>
-	</div>
+	
+		</Tabs.Content>
+	</Tabs.Root>	
 </div>
-
-<style>
-	.blog-content {
-		scrollbar-width: thin;
-	}
-</style>
--- svelte-kit/src/routes/dashboard/Event.svelte
+++ svelte-kit/src/routes/dashboard/Event.svelte
@@ -1,29 +1,47 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import * as Card from "$lib/components/ui/card";
 	import type { EventType } from '$lib/models/Event';
 	import { Trash } from 'lucide-svelte';
+	import { Button } from '$lib/components/ui/button';
+	import {Separator} from '$lib/components/ui/separator';
 
 	export let event: EventType;
 </script>
 
-<li>
-	<a class="flex" href="/dashboard/events/{event._id}">
-		<h3>{event.title}</h3>
-		<p>{event.description}</p>
-		<p>Date: {event.startDate}</p>
-		<p>Prix: {event.price}</p>
-		<p>Tag: {event.tag}</p>
-	</a>
-	<form
-		action="/dashboard/events/?/deleteEvent"
-		method="POST"
-		use:enhance={({ cancel }) => {
-			if (!confirm('Voulez-vous vraiment supprimer cet évènement ?')) cancel();
-		}}
-	>
-		<input type="hidden" name="id" value={event._id} />
-		<button>
-			<Trash />
-		</button>
-	</form>
-</li>
+<Card.Root>
+	<Card.Header>
+		<Card.Title>{event.title}</Card.Title>
+		<Card.Description>
+		{#if event.tag}
+			{event.tag}
+		{/if}
+		</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		Description :
+		{event.description}
+		<br />
+		<br />
+		Prix : {event.price}€
+	</Card.Content>
+	<Card.Footer class="flex justify-between">
+		<span>Du <b>{new Date(event.startDate).toLocaleDateString('fr-FR')}</b> au <b>{new Date(event.endDate).toLocaleDateString('fr-FR')}</b></span>
+		<div class="flex">
+			<form
+			action="/dashboard/events/?/deleteEvent"
+			method="POST"
+			use:enhance={({ cancel }) => {
+				if (!confirm('Voulez-vous vraiment supprimer cet évènement ?')) cancel();
+			}}
+		>
+			<input type="hidden" name="id" value={event._id} />
+			<Button variant="ghost" type="submit">
+				<Trash />
+			</Button>
+		</form>
+		<Separator orientation="vertical" />
+		<Button href="/dashboard/events/{event._id}" variant="ghost">Modifier</Button>
+		</div>
+	</Card.Footer>
+</Card.Root>

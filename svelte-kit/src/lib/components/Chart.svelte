159b73efd65aee56ed05af4721fca8209536e69b
hugomztl <hugo.mazzitelli@viacesi.fr>
2025-01-24 12:27:14 +0100

    Add PointElement registration for enhanced chart rendering in Chart component

--- svelte-kit/src/lib/components/Chart.svelte
+++ svelte-kit/src/lib/components/Chart.svelte
@@ -1,10 +1,10 @@
 <!-- src/lib/components/Chart.svelte -->
 <script lang="ts">
     import { Bar, Line } from 'svelte-chartjs';
-    import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, LineElement, CategoryScale, LinearScale } from 'chart.js';
+    import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js';
   
     // Enregistrement des composants nécessaires de Chart.js
-    ChartJS.register(Title, Tooltip, Legend, BarElement, LineElement, CategoryScale, LinearScale);
+    ChartJS.register(Title, Tooltip, Legend, BarElement, LineElement, CategoryScale, LinearScale, PointElement);
   
     export let type: 'bar' | 'line';
     export let data: any;

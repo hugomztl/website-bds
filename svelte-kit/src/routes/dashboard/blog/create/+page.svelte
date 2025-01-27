8506a19fb84598ef78e9d276a38b142223b5a08c
hugomztl <hugo.mazzitelli@viacesi.fr>
2025-01-27 15:28:49 +0100

    Refactor blog post creation form to use UI components and add author field

--- svelte-kit/src/routes/dashboard/blog/create/+page.svelte
+++ svelte-kit/src/routes/dashboard/blog/create/+page.svelte
@@ -1,20 +1,37 @@
 <script>
 	import { enhance } from '$app/forms';
+	import { Label } from "$lib/components/ui/label";
+	import { Textarea } from "$lib/components/ui/textarea";
+	import { Input } from "$lib/components/ui/input";
+	import { Button } from "$lib/components/ui/button";
+	import Footer from '$lib/components/footer.svelte';
+
+	export let data;
 </script>
 
-<div class="container mx-auto mt-[5%]">
+
+
+<div class="container my-[5%]">
 	<form method="POST" action="/dashboard/blog/?/createPost" use:enhance>
 		<h2 class="h2">Créer un nouveau post</h2>
 		<div>
-			<label for="title">Titre:</label>
-			<input class="input" type="text" name="title" required />
+			<Label for="title">Titre</Label>
+			<Input class="input" type="text" name="title" required />
 		</div>
 		<div>
-			<label for="content">Contenu:</label>
-			<textarea class="textarea form-textarea" name="content" required></textarea>
+			<Label for="author">Auteur</Label>
+			<Input class="w-[20vh]" type="text" name="author" value={data.session?.user?.name + " (Vous)"} disabled />
 		</div>
-		<button type="submit" class="btn variant-filled-success variant-outline-primary">
-			Créer le post
-		</button>
+		<div>
+			<Label for="content">Contenu</Label>
+			<Textarea class="textarea form-textarea h-[60vh]" name="content" required/>
+		</div>
+		<div class="flex justify-center">
+			<Button type="submit" class="mt-5">
+				Publier
+			</Button>
+		</div>
 	</form>
 </div>
+
+<Footer/>

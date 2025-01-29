3d462e084b549b6ab98affab8dd3ea288c7f7f6b
hugomztl <hugo.mazzitelli@viacesi.fr>
2025-01-29 10:43:48 +0100

    Add ClubCard component and update dashboard to display clubs using the new card layout

---
+++ svelte-kit/src/lib/components/ClubCard.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index';
+	import { Button } from '$lib/components/ui/button/index';
+	import { SquarePen } from 'lucide-svelte';
+
+	export let club;
+	//export let user;
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title>{club.name}</Card.Title>
+		<Card.Description>
+			{#if club.owner}
+				Président :
+				<Button href="/profile/{club.owner._id}" variant="link" class="px-0">
+					{club.owner.fullName}
+				</Button>
+			{:else}
+				<p>Actuellement sans président</p>
+			{/if}</Card.Description
+		>
+	</Card.Header>
+	<Card.Content>
+		{club.description}
+	</Card.Content>
+	<Card.Footer class="flex justify-end">
+		<!-- {#if user.isAdmin} -->
+		<Button href={`/clubs/${club._id}/manage`} variant="ghost"
+			><SquarePen class="mr-1" />Modifier</Button
+		>
+		<!-- {:else}
+			<Button href={`/clubs/${club._id}`} variant="ghost"><SquarePen class="mr-1" />Voir</Button>
+		{/if} -->
+	</Card.Footer>
+</Card.Root>

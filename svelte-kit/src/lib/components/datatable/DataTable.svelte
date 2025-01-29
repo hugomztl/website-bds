51a36e71ed7a3d0c2b3fde3477ea9a33e72e7f94
hugomztl <hugo.mazzitelli@viacesi.fr>
2025-01-29 17:01:18 +0100

    Add row selection functionality to DataTable component with checkbox support

--- svelte-kit/src/lib/components/datatable/DataTable.svelte
+++ svelte-kit/src/lib/components/datatable/DataTable.svelte
@@ -4,20 +4,26 @@
  - formattage des cellules
  - tri/filtres ✓
  - pagination ✓
- - champ de recherche
+ - champ de recherche ✓
  - selection de ligne
  - actions (CRUD)
   -->
 
 <script lang="ts">
-	import { createTable, Render, Subscribe } from 'svelte-headless-table';
-	import { addPagination, addSortBy, addTableFilter } from 'svelte-headless-table/plugins';
+	import { createTable, Render, Subscribe, createRender } from 'svelte-headless-table';
+	import {
+		addPagination,
+		addSortBy,
+		addTableFilter,
+		addSelectedRows
+	} from 'svelte-headless-table/plugins';
 	import { readable } from 'svelte/store';
 	import * as Table from '$lib/components/ui/table';
 	import { Input } from '$lib/components/ui/input';
 	import { Button } from '$lib/components/ui/button';
 	import { ArrowUpDown, ChevronLeft, ChevronRight, UserSearch } from 'lucide-svelte/icons';
 	import type { UserType } from '$lib/models/User';
+	import DataTableCheckbox from './DataTableCheckbox.svelte';
 
 	export let data: UserType[];
 	export let userCount: number;
@@ -27,10 +33,35 @@
 		sort: addSortBy(),
 		filter: addTableFilter({
 			fn: ({ filterValue, value }) => value.toLowerCase().includes(filterValue.toLowerCase())
-		})
+		}),
+		select: addSelectedRows()
 	});
 
 	const columns = table.createColumns([
+		table.column({
+			accessor: '_id',
+			header: (_, { pluginStates }) => {
+				const { allPageRowsSelected } = pluginStates.select;
+				return createRender(DataTableCheckbox, {
+					checked: allPageRowsSelected,
+					mongoId: 'Identifiant'
+				});
+			},
+			cell: ({ row }, { pluginStates }) => {
+				const { getRowState } = pluginStates.select;
+				const { isSelected } = getRowState(row);
+
+				return createRender(DataTableCheckbox, {
+					checked: isSelected,
+					mongoId: row.original._id
+				});
+			},
+			plugins: {
+				sort: {
+					disable: true
+				}
+			}
+		}),
 		table.column({
 			accessor: 'fullName',
 			header: 'Nom Complet'
@@ -46,33 +77,35 @@
 		table.column({
 			accessor: 'admin',
 			header: 'Administrateur',
-			cell: ({ row }) => (row.original.admin ? 'Oui' : 'Non') //FIXME: je crois que c'est pas la bonne façon de faire
-		}),
-		table.column({
-			accessor: 'license',
-			header: 'Licence',
-			cell: ({ row }) => (row.original.license ? 'Oui' : 'Non') //FIXME: je crois que c'est pas la bonne façon de faire
-		}),
-		table.column({
-			accessor: 'dateInscription',
-			header: "Date d'Inscription",
-			cell: ({ row }) => new Date(row.original.dateInscription).toLocaleDateString('fr-FR') //FIXME: je crois que c'est pas la bonne façon de faire
-		}),
-		table.column({
-			accessor: '_id',
-			header: 'Identifiant',
+			cell: ({ row }) => (row.original.admin ? 'Oui' : 'Non'), //FIXME: je crois que c'est pas la bonne façon de faire
 			plugins: {
 				sort: {
 					disable: true
 				}
 			}
+		}),
+		table.column({
+			accessor: 'license',
+			header: 'Licence',
+			cell: ({ row }) => (row.original.license ? 'Oui' : 'Non'), //FIXME: je crois que c'est pas la bonne façon de faire
+			plugins: {
+				sort: {
+					disable: true
+				}
+			}
+		}),
+		table.column({
+			accessor: 'dateInscription',
+			header: "Date d'Inscription",
+			cell: ({ row }) => new Date(row.original.dateInscription).toLocaleDateString('fr-FR') //FIXME: je crois que c'est pas la bonne façon de faire
 		})
 	]);
 
-	const { headerRows, pageRows, tableAttrs, tableBodyAttrs, pluginStates } =
+	const { headerRows, pageRows, tableAttrs, tableBodyAttrs, pluginStates, flatColumns, rows } =
 		table.createViewModel(columns);
 	const { hasNextPage, hasPreviousPage, pageIndex } = pluginStates.page;
 	const { filterValue } = pluginStates.filter;
+	const { selectedDataIds } = pluginStates.select;
 </script>
 
 <div class="my-5 flex items-center justify-between">
@@ -97,8 +130,8 @@
 						<Table.Row>
 							{#each headerRow.cells as cell (cell.id)}
 								<Subscribe attrs={cell.attrs()} let:attrs props={cell.props()} let:props>
-									<Table.Head {...attrs}>
-										{#if cell.id != '_id'}
+									<Table.Head {...attrs} class="[&:has([role=checkbox])]:pl-3">
+										{#if cell.id != '_id' && cell.id != 'admin' && cell.id != 'license'}
 											<Button variant="ghost" on:click={props.sort.toggle}>
 												<Render of={cell.render()} />
 												<ArrowUpDown class={'ml-2 h-4 w-4'} />
@@ -116,11 +149,19 @@
 			<Table.Body {...$tableBodyAttrs}>
 				{#each $pageRows as row (row.id)}
 					<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-						<Table.Row {...rowAttrs}>
+						<Table.Row {...rowAttrs} data-state={$selectedDataIds[row.id] && 'selected'}>
 							{#each row.cells as cell (cell.id)}
 								<Subscribe attrs={cell.attrs()} let:attrs>
 									<Table.Cell {...attrs}>
-										<Render of={cell.render()} />
+										{#if cell.id === '_id'}
+											<div class="-ml-1 flex">
+												<Render of={cell.render()} />
+											</div>
+										{:else}
+											<div class="ml-4 flex">
+												<Render of={cell.render()} />
+											</div>
+										{/if}
 									</Table.Cell>
 								</Subscribe>
 							{/each}
@@ -130,18 +171,24 @@
 			</Table.Body>
 		</Table.Root>
 	</div>
-	<div class="flex items-center justify-center space-x-5 py-5">
-		<Button
-			variant="outline"
-			size="sm"
-			on:click={() => ($pageIndex = $pageIndex - 1)}
-			disabled={!$hasPreviousPage}><ChevronLeft class="mr-1" />Précédent</Button
-		>
-		<Button
-			variant="outline"
-			size="sm"
-			disabled={!$hasNextPage}
-			on:click={() => ($pageIndex = $pageIndex + 1)}>Suivant<ChevronRight class="ml-1" /></Button
-		>
+	<div class="flex items-center justify-between space-y-5">
+		<div class="text-muted-foreground text-sm">
+			{Object.keys($selectedDataIds).length} sur{' '}
+			{$rows.length} ligne(s) selectionées
+		</div>
+		<div class="flex justify-between space-x-5">
+			<Button
+				variant="outline"
+				size="sm"
+				on:click={() => ($pageIndex = $pageIndex - 1)}
+				disabled={!$hasPreviousPage}><ChevronLeft class="mr-1" />Précédent</Button
+			>
+			<Button
+				variant="outline"
+				size="sm"
+				disabled={!$hasNextPage}
+				on:click={() => ($pageIndex = $pageIndex + 1)}>Suivant<ChevronRight class="ml-1" /></Button
+			>
+		</div>
 	</div>
 </div>
---
+++ svelte-kit/src/lib/components/datatable/DataTableCheckbox.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Checkbox } from '$lib/components/ui/checkbox';
+	import { Label } from '$lib/components/ui/label';
+	import type { Writable } from 'svelte/store';
+
+	export let checked: Writable<boolean>;
+	export let mongoId: Writable<string>;
+</script>
+
+<div class="flex items-center space-x-2">
+	<Checkbox bind:checked={$checked} id="id" />
+	<Label for="id">{mongoId}</Label>
+</div>
